a88c3e5f00b8c2ac75482a5aba8bcefdcf875131
Jose Torres <jt227b@gmail.com>
2019-09-03 09:58:13 -0500

    Setup local storage function to app

--- app.js
+++ app.js
@@ -44,11 +44,28 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in Local Storage
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear Input
   taskInput.value = "";
 
   // Always prevent default from happening by:
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Remove Task X Function
